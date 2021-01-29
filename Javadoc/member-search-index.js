33064fd51fe1911044a76d66184ce1bffe7768e7
KyshynetsVlad <72409370+KyshynetsVlad@users.noreply.github.com>
2021-01-30 00:08:34 +0300

    Delete Javadoc directory

--- Javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"domain","c":"Exercise","l":"Calculate(int[])"},{"p":"domain","c":"Exercise","l":"Exercise()","url":"%3Cinit%3E()"},{"p":"test","c":"TestResult","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"test","c":"TestResult","l":"TestResult()","url":"%3Cinit%3E()"}]
--- Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"domain","l":"Exercise"},{"p":"test","l":"TestResult"}]
